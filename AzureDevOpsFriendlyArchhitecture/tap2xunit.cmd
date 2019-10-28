6edb5657df6919f67b251fa9e1f2026999c1159c
dmarkovtsev <maverick-@users.noreply.github.com>
2019-10-28 11:07:31 +0300

    PublishBuildArtifacts task added to collect all TAP, TRP, LOG files after test execution

--- AzureDevOpsFriendlyArchhitecture/tap2xunit.cmd
+++ AzureDevOpsFriendlyArchhitecture/tap2xunit.cmd
@@ -4,8 +4,21 @@ FOR /D %%G in ("*") DO (
     IF EXIST "%%~nxG\last.tap" (
         ECHO File exists %%~nxG\last.tap
         CALL tap --no-coverage -Rxunit - < %%~nxG\last.tap > results\%%~nxG.xml
+        copy %%~nxG\last.tap reports\%%~nxG.tap
     ) else (
         ECHO File not found %%~nxG\last.tap
+    )
+    IF EXIST "%%~nxG\%%~nxG.trp" (
+        ECHO File exists %%~nxG\%%~nxG.trp
+        copy %%~nxG\%%~nxG.trp reports\%%~nxG.trp
+    ) else (
+        ECHO File not found %%~nxG\%%~nxG.trp
+    )
+    IF EXIST "%%~nxG\summary.log" (
+        ECHO File exists %%~nxG\summary.log
+        copy %%~nxG\summary.log reports\%%~nxG.log
+    ) else (
+        ECHO File not found %%~nxG\summary.log
     )
 )
 ECHO Results converted
